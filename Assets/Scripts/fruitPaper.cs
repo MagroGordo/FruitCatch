641857686dbdf8248ad374587f5766913bdb537c
MagroGordo <amgrogordo123@gmail.com>
2024-05-20 17:27:03 +0100

    estagio

--- Assets/Scripts/fruitPaper.cs
+++ Assets/Scripts/fruitPaper.cs
@@ -2,16 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class fruitPaper : MonoBehaviour
 {
-    [SerializeField] private Animator anim_;
+    [SerializeField] private GameObject paper;
+    [SerializeField] private TextMeshProUGUI text;
 
-    private bool hidden;
+    private bool visible;
 
     private void Start()
     {
-        anim_ = GetComponent<Animator>();   
-        hidden = anim_.GetBool("hidden");
+        paper = GameObject.Find("Fruit Count");
+        text = FindObjectOfType<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        text.text = "lemon x17\n" +
+            "banana x20\n" +
+            "ornage x 1\n" +
+            "strawberry x6\n" +
+            "watermelon x9\n" +
+            "melon x5\n" +
+            "kiwi x0\n" +
+            "pineapple x8";
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            visible = !visible;
+            paper.SetActive(visible);
+        }
     }
 }
